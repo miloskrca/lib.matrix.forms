ecfc64fcaa99487d4706b916e288d7475e5c3dee
Miloš Krsmanović <milos.krsman@gmail.com>
2014-05-03 13:51:42 +0200

    * Updated Matrix to work with Object type

--- src/rs/etf/km123247m/Matrix/Matrix.java
+++ src/rs/etf/km123247m/Matrix/Matrix.java
@@ -8,18 +8,17 @@ import java.util.ArrayList;
  *
  * package: rs.etf.km123247m.Matrix
  */
-public class Matrix<T> {
+public class Matrix {
     private final int rowNumber;
     private final int columnNumber;
-    private ArrayList<ArrayList<T>> matrix;
+    private ArrayList<ArrayList<Object>> matrix;
 
-    @SuppressWarnings("unchecked")
     public Matrix(int columnNumber, int rowNumber) {
         this.columnNumber = columnNumber;
         this.rowNumber = rowNumber;
-        this.matrix = new ArrayList();
+        this.matrix = new ArrayList<ArrayList<Object>>();
     }
-    public Matrix(ArrayList<ArrayList<T>> matrix) {
+    public Matrix(ArrayList<ArrayList<Object>> matrix) {
         this.columnNumber = matrix.size();
         this.rowNumber = matrix.size() > 0 ? matrix.get(0).size(): 0;
         this.matrix = matrix;
@@ -33,21 +32,22 @@ public class Matrix<T> {
         return rowNumber;
     }
 
-    public ArrayList<ArrayList<T>> getMatrix() {
+    public ArrayList<ArrayList<Object>> getMatrix() {
         return matrix;
     }
 
-    public void set(int col, int row, T t) throws Exception {
+    public void set(int col, int row, Object t) throws Exception {
         if(col >= columnNumber) {
             throw new Exception("Column number doesn't exist!");
         }
         if(row >= rowNumber) {
             throw new Exception("Row number doesn't exist!");
         }
+        // @TODO: fix IndexOutOfBounds exception
         this.getMatrix().get(col).set(row, t);
     }
 
-    public T get(int col, int row) throws Exception {
+    public Object get(int col, int row) throws Exception {
         if(col >= columnNumber) {
             throw new Exception("Column number doesn't exist!");
         }
